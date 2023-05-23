4a2f3c41715ab4f8b587074b698d633d84e82b6c
Elhambasir <108110651+Elhambasir@users.noreply.github.com>
2023-05-23 14:31:47 +0430

    Add Navlink to Nav.js

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,3 +1,18 @@
-const Nav = () => <nav />;
+import { NavLink } from 'react-router-dom';
+import styles from '../styles/nav.module.css';
+
+const links = [
+  { path: '/', text: 'Rockets' },
+  { path: 'test', text: 'test' },
+];
+const Nav = () => (
+  <>
+    {links.map((link) => (
+      <li key={link.text}>
+        <NavLink className={styles.noDecoration} to={link.path}>{link.text}</NavLink>
+      </li>
+    ))}
+  </>
+);
 
 export default Nav;
