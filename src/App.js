4477f32a8b27bf81ba49e247bf18cd57bac6aea8
Elhambasir <108110651+Elhambasir@users.noreply.github.com>
2023-05-23 14:43:17 +0430

    Add rout for my profile

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Nav from './components/Nav';
 import './App.css';
+import MyProfile from './components/MyProfile';
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Nav />} />
+          <Route path="/MyProfile" element={<MyProfile />} />
           <Route path="*" element={<h3>No Match</h3>} />
         </Routes>
       </BrowserRouter>
