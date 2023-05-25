d91a8b7b1e2df90a11677d2cacda392b245a3eed
Abdullah <abdullahkhrief@gmail.com>
2023-05-25 17:50:57 +0300

    Filter has been added to the profile to filter the reserved Rockets

--- src/components/MyProfile.jsx
+++ src/components/MyProfile.jsx
@@ -1,9 +1,44 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+import { Container, ListGroup } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.css';
 
-function MyProfile() {
+function Mission() {
+  const mission = useSelector((state) => state.missions);
+  const myMission = mission.missions.filter((item) => item.reserved === true);
+  if (myMission.length === 0) return (<Container>You have not joined any Mission.</Container>);
   return (
-    <div>MyProfile</div>
+    <Container>
+      <ListGroup as="ol" numbered>
+        {myMission.map((item) => (
+          <ListGroup.Item as="li" key={item.id}>{item.name}</ListGroup.Item>
+        ))}
+      </ListGroup>
+    </Container>
   );
 }
+
+const Rocket = () => {
+  const Rocket = useSelector((state) => state.rockets);
+  const myMission = Rocket.RocketList.filter((item) => item.reserved === true);
+  if (myMission.length === 0) return (<Container>You have not reserved any Rocket.</Container>);
+  return (
+    <Container>
+      <ListGroup as="ol" numbered>
+        {myMission.map((item) => (
+          <ListGroup.Item as="li" key={item.id}>{item.name}</ListGroup.Item>
+        ))}
+      </ListGroup>
+    </Container>
+  );
+}
+
+const MyProfile = () => {
+  return(<>
+  <Mission/>
+  <Rocket/>
+  </>)
+}
+
 
 export default MyProfile;
--- src/components/RocketsList.jsx
+++ src/components/RocketsList.jsx
@@ -10,7 +10,7 @@ const RocketList = () => {
   } = useSelector((store) => store.rockets);
   useEffect(() => {
     dispatch(getAPI());
-  }, [dispatch]);
+  }, [error]);
   if (isLoading === true) {
     return <div>Loading</div>;
   } if (isLoading === false) {
