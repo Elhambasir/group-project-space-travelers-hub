a76de2a839af905933a9147e2922402e270825ad
Elhambasir <108110651+Elhambasir@users.noreply.github.com>
2023-05-25 09:29:01 +0430

    Resolve conflicts

--- src/index.js
+++ src/index.js
@@ -3,8 +3,8 @@ import ReactDOM from 'react-dom/client';
 // eslint-disable-next-line import/no-extraneous-dependencies
 import { Provider } from 'react-redux';
 import store from './redux/store';
-import './index.css';
 import App from './App';
+import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
