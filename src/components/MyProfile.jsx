d8406cd8ff353210d95e7b988a94df1bcff31b86
Elhambasir <108110651+Elhambasir@users.noreply.github.com>
2023-05-25 17:38:13 +0430

    Update MyProfile.jsx
    Render my profile item from state

--- src/components/MyProfile.jsx
+++ src/components/MyProfile.jsx
@@ -1,8 +1,20 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+import { Container, ListGroup } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.css';
 
 function MyProfile() {
+  const mission = useSelector((state) => state.missions);
+  const myMission = mission.missions.filter((item) => item.reserved === true);
+  if (myMission.length === 0) return (<Container>You have not joined any Mission.</Container>);
   return (
-    <div>MyProfile</div>
+    <Container>
+      <ListGroup as="ol" numbered>
+        {myMission.map((item) => (
+          <ListGroup.Item as="li" key={item.id}>{item.name}</ListGroup.Item>
+        ))}
+      </ListGroup>
+    </Container>
   );
 }
 
